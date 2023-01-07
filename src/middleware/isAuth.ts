edd656efade715a6bb784d96bf173d4c2d113d47
Serey Roth <sereyratanakroth@gmail.com>
2023-01-06 17:06:51 -0800

    Add custom error to isAuth middleware

--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -3,6 +3,7 @@ import { MiddlewareFn } from "type-graphql";
 
 export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
     if (!context.req.session.userId) {
+        throw new Error('unauthenticated')
     }
 
     return next();
